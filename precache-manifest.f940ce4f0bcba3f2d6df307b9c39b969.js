1cf7947d5c02d17ca6b64b96909a4c9ff47cee6d
fakeshotz <fakeshotz100@gmail.com>
2020-07-25 21:15:51 -0400

    Updates

---
+++ precache-manifest.f940ce4f0bcba3f2d6df307b9c39b969.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd7f4d60d59d556afe7106d5fef0317",
+    "url": "/bankapp/index.html"
+  },
+  {
+    "revision": "6b0af210885ab493ff10",
+    "url": "/bankapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "71350da8493cd9975af0",
+    "url": "/bankapp/static/js/2.584ac706.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bankapp/static/js/2.584ac706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b0af210885ab493ff10",
+    "url": "/bankapp/static/js/main.bdc511d3.chunk.js"
+  },
+  {
+    "revision": "a281fadf79282d0916ad",
+    "url": "/bankapp/static/js/runtime-main.f96143e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bankapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c99e8042e4d0d4806e1923108e1fd56b",
+    "url": "/bankapp/static/media/redmain.c99e8042.jpg"
+  },
+  {
+    "revision": "11242bb53b17b0d6947d1e0963d9db79",
+    "url": "/bankapp/static/media/scotia.11242bb5.jpg"
+  },
+  {
+    "revision": "ecff2ead7b50ca6c5775a03e0a49a037",
+    "url": "/bankapp/static/media/t1.ecff2ead.jpg"
+  },
+  {
+    "revision": "88382c5717e6d200a2fc597931efc9c5",
+    "url": "/bankapp/static/media/t2.88382c57.gif"
+  },
+  {
+    "revision": "4c659677b8a475f6585c13d1ea17e7bf",
+    "url": "/bankapp/static/media/t3.4c659677.jpg"
+  },
+  {
+    "revision": "ae8ab370aef17eb5ab2481a39bf9daf0",
+    "url": "/bankapp/static/media/t4.ae8ab370.jpg"
+  },
+  {
+    "revision": "79b7d5f2450a76f8770a1b8984d29154",
+    "url": "/bankapp/static/media/t5.79b7d5f2.jpg"
+  },
+  {
+    "revision": "cdb911bb98298dcb3df509dbba53cb08",
+    "url": "/bankapp/static/media/t6.cdb911bb.jpg"
+  },
+  {
+    "revision": "34383e6dec697114dc7f12d101723d5f",
+    "url": "/bankapp/static/media/t7.34383e6d.png"
+  },
+  {
+    "revision": "1c1f3541e01f508b60f20410f3cbe2f9",
+    "url": "/bankapp/static/media/t8.1c1f3541.jpg"
+  }
+]);
